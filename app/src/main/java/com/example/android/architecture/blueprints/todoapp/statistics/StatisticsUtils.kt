0948e6e75068a0694eb8cb0e6fd46f83722c45a6
DESKTOP-AMPP2BT\Mking <msit26055@gmail.com>
2021-11-20 18:50:17 +0330

    create first test

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtils.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtils.kt
@@ -23,16 +23,19 @@ import com.example.android.architecture.blueprints.todoapp.data.Task
  */
 internal fun getActiveAndCompletedStats(tasks: List<Task>?): StatsResult {
 
-    return if (tasks == null || tasks.isEmpty()) {
-        StatsResult(0f, 0f)
-    } else {
-        val totalTasks = tasks.size
-        val numberOfActiveTasks = tasks.count { it.isActive }
-        StatsResult(
-            activeTasksPercent = 100f * numberOfActiveTasks / tasks.size,
-            completedTasksPercent = 100f * (totalTasks - numberOfActiveTasks) / tasks.size
-        )
+    if (tasks.isNullOrEmpty()) {
+        return StatsResult(0f, 0f)
     }
+
+    val totalTasks = tasks.size
+    val numberOfActiveTasks = tasks.count { it.isActive }
+    val activePercent = 100 * numberOfActiveTasks / totalTasks
+    val completePercent = 100 * (totalTasks - numberOfActiveTasks) / totalTasks
+
+    return StatsResult(
+            activeTasksPercent = activePercent.toFloat(),
+            completedTasksPercent = completePercent.toFloat()
+    )
 }
 
 data class StatsResult(val activeTasksPercent: Float, val completedTasksPercent: Float)
